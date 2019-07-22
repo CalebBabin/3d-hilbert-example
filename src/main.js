78c6bc37a65a73fd4c757de36cf2e3027c0ea899
Caleb Babin <object.caleb@gmail.com>
2019-07-21 21:10:44 -0500

    enable anti-aliasing

--- src/main.js
+++ src/main.js
@@ -55,7 +55,9 @@ function init() {
 
     raycaster = new THREE.Raycaster();
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
